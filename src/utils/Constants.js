c7e831b4ad33dc612c6c61f96b4320efc2919aa2
Oseme Odigie <osemeodigie@yahoo.com>
2019-06-14 13:34:56 +0100

    feat: added a Constants file to manage constants in the app

---
+++ src/utils/Constants.js
@@ -0,0 +1,20 @@
+/**
+ * List of constants on the App.
+ */
+const Constants = {
+    ENDPOINTS: {
+        LOG_IN: '',
+        LOG_OUT: '',
+        DOCUMENTS: '',
+        ANNOTATIONS: '',
+        USERS: '',
+        NAMED_ENTITIES: ''
+    },
+    USER_ROLES: {
+        ANALYST: 0,
+        MANAGER: 1
+    },
+    REFRESH_RATE: 1 * 60 // 10 minutes
+}
+
+export default Constants;
